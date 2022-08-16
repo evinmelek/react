fafbc9d06a1659ce74e5f918b01d249a3b87a817
evinmelek <evinmelek06@gmail.com>
2022-08-16 09:52:36 +0300

    initial commit

---
+++ src/Info.js
@@ -0,0 +1,12 @@
+import "./css/info.css";
+
+const Info = () => {
+	return (
+		<div className="info">
+			<div className="player"> X </div>
+			<div className="player"> O </div>
+		</div>
+	)
+}
+
+export default Info;
